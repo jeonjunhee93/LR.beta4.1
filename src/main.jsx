435a1f4c07e738c4802f3af8bcee3d22e9a7bf46
jeonjunhee93 <lllllljz@naver.com>
2025-06-13 06:36:11 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LifeRPG from "./LifeRPG";
-import "./index.css";
+import "./index.css"; // 또는 생략 가능
 
-ReactDOM.createRoot(document.getElementById("root")).render(<LifeRPG />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LifeRPG />
+  </React.StrictMode>
+);
